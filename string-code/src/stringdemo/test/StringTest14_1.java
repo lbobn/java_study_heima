2df23d0c14e5d4f492373822aa919b54c1071b31
lubb <2820843645@qq.com>
2022-12-30 21:53:34 +0800

    string-study

---
+++ string-code/src/stringdemo/test/StringTest14_1.java
@@ -0,0 +1,14 @@
+package stringdemo.test;
+
+public class StringTest14_1 {
+    public static void main(String[] args) {
+
+        /*给定两个字符串，A和B。
+        A的旋转操作就是将A最左边的字符移动到最右边。
+        例如，若A = 'abcde '，在移动一次之后结果就是' bcdea '如果在若干次调整操作之后，A能变成B，那么返回True。
+        如果不能匹配成功，则返回false
+        */
+        String strA = "abcde";
+        String strB = "cdeab";
+    }
+}
---
+++ string-code/src/stringdemo/test/StringTest14_2.java
@@ -0,0 +1,7 @@
+package stringdemo.test;
+
+public class StringTest14_2 {
+    public static void main(String[] args) {
+        //第二种方法
+    }
+}

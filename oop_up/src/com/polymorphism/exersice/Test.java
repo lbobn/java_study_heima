d17bd31e9f8dca1b03c8a3e152d8d1f4a6278707
lubb <2820843645@qq.com>
2023-01-12 21:18:01 +0800

    变更

--- oop_up/src/com/polymorphism/exersice/Test.java
+++ oop_up/src/com/polymorphism/exersice/Test.java
@@ -28,10 +28,10 @@ public class Test {
 				功能：喂养宠物猫，something表示喂养的东西
 		生成空参有参构造，set和get方法
 	4.定义测试类(完成以下打印效果):
-		keepPet(Dog dog,String somethind)方法打印内容如下：
+		keepPet(Dog dog,String something)方法打印内容如下：
 			年龄为30岁的老王养了一只黑颜色的2岁的狗
 			2岁的黑颜色的狗两只前腿死死的抱住骨头猛吃
-		keepPet(Cat cat,String somethind)方法打印内容如下：
+		keepPet(Cat cat,String something)方法打印内容如下：
 			年龄为25岁的老李养了一只灰颜色的3岁的猫
 			3岁的灰颜色的猫眯着眼睛侧着头吃鱼
 	5.思考：

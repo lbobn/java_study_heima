46f90d876c2787ec096cfcd3ecbe7d1f47b39451
lubb <2820843645@qq.com>
2023-02-12 23:50:23 +0800

    alter

---
+++ algorithms-code/src/com/myarrays/ArraysDemo2.java
@@ -0,0 +1,19 @@
+package com.myarrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArraysDemo2 {
+    public static void main(String[] args) {
+        Integer[] arr = {2, 3, 1, 5, 6, 7, 8, 4, 9};
+
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+
+        System.out.println(Arrays.toString(arr));
+    }
+}

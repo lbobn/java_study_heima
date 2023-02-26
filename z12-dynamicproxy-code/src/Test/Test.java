424934bd98c9c9f3c17ac04bc800cece6eb75787
lubb <2820843645@qq.com>
2023-02-26 21:54:39 +0800

    变更

---
+++ z12-dynamicproxy-code/src/Test/Test.java
@@ -0,0 +1,13 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+}

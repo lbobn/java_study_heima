80d374786b8a7b630a093141b27446082d0248c0
lubb <2820843645@qq.com>
2023-02-27 21:45:33 +0800

    变更

--- z08-io-code/src/test/crawertest/Test1.java
+++ z08-io-code/src/test/crawertest/Test1.java
@@ -1,11 +1,16 @@
 package test.crawertest;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -55,8 +60,62 @@ public class Test1 {
                 girlName.add(s1);
             }
         }
+        //获取数据
+        ArrayList<String> info = getInfo(familyName, boyName, girlName, 20, 15);
+        Collections.shuffle(info);
+        System.out.println(info);
 
+        //写入本地文件
+        BufferedWriter bw = new BufferedWriter(new FileWriter("z08-io-code\\src\\test\\crawertest\\names.txt"));
+        for (String s : info) {
+            bw.write(s);
+            bw.newLine();
+        }
+        bw.close();
 
+    }
+
+    /*
+     * 获取姓名
+     * */
+    public static ArrayList<String> getInfo(ArrayList<String> familyName
+            , ArrayList<String> boyName, ArrayList<String> girlName, int boyCount, int girlCount) {
+        //1.生成男生不重复的名字
+        HashSet<String> boyhs = new HashSet<>();
+        while (true) {
+            if (boyhs.size() == boyCount) {
+                break;
+            }
+            Collections.shuffle(familyName);
+            Collections.shuffle(boyName);
+            boyhs.add(familyName.get(0) + boyName.get(0));
+        }
+        //2.生成女生不重复的名字
+        HashSet<String> girlhs = new HashSet<>();
+        while (true) {
+            if (girlhs.size() == girlCount) {
+                break;
+            }
+            Collections.shuffle(familyName);
+            Collections.shuffle(girlName);
+            girlhs.add(familyName.get(0) + girlName.get(0));
+        }
+
+        //生成数据
+        //1.男生
+        ArrayList<String> list = new ArrayList<>();
+        Random r = new Random();
+
+        for (String boyh : boyhs) {
+            int age = r.nextInt(10) + 18;
+            list.add(boyh + "-男-" + age);
+        }
+        //1.女生
+        for (String girlh : girlhs) {
+            int age = r.nextInt(10) + 18;
+            list.add(girlh + "-女-" + age);
+        }
+        return list;
     }
 
     /*

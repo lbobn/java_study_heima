e9010dc44c41f2ef2108b4d7d5c7e16f63a34903
lubb <2820843645@qq.com>
2023-03-13 22:15:22 +0800

    changes

--- z12-dynamicproxy-code/src/Test.java
+++ z12-dynamicproxy-code/src/Test.java
@@ -14,12 +14,14 @@ public class Test {
 
 
         //1. 获取代理的对象
-        BigStar bigStar = new BigStar("坤哥");
+        BigStar bigStar = new BigStar("蔡徐坤");
         Star proxy = ProxyUtil.createProxy(bigStar);
 
         //2. 调用唱歌的方法
         String result = proxy.sing("只因你太美");
         System.out.println(result);
+        //调用跳舞的方法
+        proxy.dance();
 
 
     }

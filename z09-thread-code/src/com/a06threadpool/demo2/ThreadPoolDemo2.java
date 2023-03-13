b0efc3cd053eb935abefca8a56b42d3859526616
lubb <2820843645@qq.com>
2023-03-13 22:11:40 +0800

    changes

--- z09-thread-code/src/com/a06threadpool/demo2/ThreadPoolDemo2.java
+++ z09-thread-code/src/com/a06threadpool/demo2/ThreadPoolDemo2.java
@@ -4,6 +4,6 @@ public class ThreadPoolDemo2 {
     public static void main(String[] args) {
         //向Java虚拟机返回可用处理器的数目
         int count = Runtime.getRuntime().availableProcessors();
-        System.out.println(count);
+        System.out.println("可用处理器数目" + count);
     }
 }
